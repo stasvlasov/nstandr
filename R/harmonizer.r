a1c9ed2e6900b7d4143fa64a65a97be48e6fff92
stasvlasov <s.vlasov@tilburguniversity.edu>
2018-01-02 00:22:33 +0700

    Fix several bugs and add new features
    Includes:
    - add progress indication for each procedure (see new option
    "progress.by" in harmonizer)
    - fixed position of "DONE" message
    - added more filters and removals (e.g., Ltda)
    - fixed trimming spaces in between of removals
    - fixed removals of the first word - now S.A.S. Ltd. will stay as
    SAS (before it deleted whole string)
    - fixed encoding and encoding detection
    - added list of legal entities from Magerman et al. (2006) but it is
    not utilized in harmonizer now

--- R/harmonizer.r
+++ R/harmonizer.r
@@ -25,7 +25,8 @@
 ##            , 'dplyr'
 ##            , 'magrittr'
 ##            , "XML"
-##              , "xml2"))
+##            , "xml2"
+##            , "readr"))
 ##     if(!require(pkg, character.only = TRUE)) {
 ##         install.packages(pkg, repos = 'http://cloud.r-project.org')
 ##         library(pkg, character.only = TRUE)
@@ -58,16 +59,41 @@ toutf <- function(str) str_conv(str, "UTF-8")
 
 
 ## Translates non-ascii symbols to its ascii equivalent
-#' @import stringr magrittr
-toascii <- function(str) {
+#' @import stringi stringr magrittr
+toascii <- function(str, enc.detect = FALSE) {
     ## utf <- "ŠŒŽšœžŸ¥µÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýÿ" %>% toutf
     utf <- system.file(package = "harmonizer") %>%
         file.path("enc/enc.txt") %>%
         readLines %>%
-        str_conv("UTF-8")
+        enc2utf8
     ascii <- "SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy"
-    chartr(utf, ascii, str)
+    if(enc.detect == "all")  # detect encoding of whole vector
+        iconv(str
+            , from = stri_enc_detect(str)[[1]]$Encoding[1]
+            , to = "UTF-8"
+            , sub = "") %>% 
+            {chartr(utf, ascii, .)} %>% 
+            iconv(to = "ASCII", sub = "")
+    else if(enc.detect == "ind")  # detect encoding of individual elements
+        lapply(str, function(el) iconv(el
+                                     , from = stri_enc_detect(el)[[1]]$Encoding[1]
+                                     , to = "UTF-8"
+                                     , sub = "")) %>%
+            unlist %>% 
+            {chartr(utf, ascii, .)} %>% 
+            iconv(to = "ASCII", sub = "")
+    else
+        enc2utf8(str) %>% 
+            {chartr(utf, ascii, .)} %>% 
+            iconv(to = "ASCII", sub = "")
 }
+
+
+## Test
+## toascii(c("fa\xE7ile"
+##         , "fa\xc3\xa7ile")
+##         , enc.detect = "ind")
+
 
 
 ## Trims whitespases
@@ -84,24 +110,19 @@ clear.in.brackets <- function(str) str_replace_all(str, "<.*>|\\(.*\\)|\\{.*\\}|
 
 ## Makes list of supstitutes given csv files with two columns (del, ins)
 #' @import magrittr data.table
-make.subs.table <- function(harm.files) {
-    harm.files %>%
-        lapply(function(file)
-            read.csv(file
-                   , header = FALSE
-                   , col.names = c("del", "ins")
-                   , as.is = TRUE
-                   , colClasses = c("character", "character")
-                   , na.strings = NULL)) %>%
-        rbindlist
-}
+## make.subs.table <- function(harm.files) {
+##     harm.files %>%
+##         lapply(function(file)
+##             read.csv(file
+##                    , header = FALSE
+##                    , col.names = c("del", "ins")
+##                    , as.is = TRUE
+##                    , colClasses = c("character", "character")
+##                    , na.strings = NULL)) %>%
+##         rbindlist
+## }
 
-## Subsitutes strings
-#' @import magrittr stringi
-standardize <- function(org.names, subs.table, add.spaces = TRUE) {
-    if(add.spaces) org.names <- paste(" ", org.names, " ")  # add spaces
-    stri_replace_all_fixed(org.names, subs.table$del, subs.table$ins, vectorize_all = FALSE) %>% return
-}
+
 
 ## Detects type of organization based on clues from 'harm.codes.list'
 #' @import magrittr stringi
@@ -125,19 +146,48 @@ harm.detect <- function(org.names, harm.codes.list, add.spaces = TRUE){
 
 
 
+## Subsitutes strings
+#' @import magrittr stringi stringr
+standardize <- function(org.names, subs.files
+                      , add.spaces = "both") {
+    for(file in subs.files) {
+        org.names %<>% trims
+        if(add.spaces == "right") org.names %<>%  paste0(" ")  # add space at the end
+        if(add.spaces == "both") org.names %<>% {paste0(" ",. , " ")}  # add space at the end
+        subs.table <- read.csv(file
+                             , header = FALSE
+                             , col.names = c("del", "ins")
+                             , as.is = TRUE
+                             , colClasses = c("character", "character")
+                             , na.strings = NULL
+                             , comment.char = "#")
+        org.names %<>% stri_replace_all_fixed(subs.table$del
+                                            , subs.table$ins
+                                            , vectorize_all = FALSE)
+    }
+    return(org.names)
+}
+
+
+
+
+
 ## Load Substitution Rules
 ## ================================================================================
 
 ## Path for files with substitutions
 harm.dir <- system.file(package = "harmonizer") %>%
     file.path("nber-pdp-harmonization")
-
+## harm.dir <- file.path("../inst/nber-pdp-harmonization")  # for testing
 
 ## Substitutions
 harm.subs.files <- c("nber-pdp-harmonization-(punctuation2_not_all-2,5,6,10).csv"
                    , "nber-pdp-harmonization-(standard_name-8).csv"
+                   , "nber-pdp-harmonization-(non_corporates_univ-8).csv"
                    , "nber-pdp-harmonization-(nameonly_main-9).csv"
                    , "nber-pdp-harmonization-(derwent_standardisation_BHH-9).csv")
+
+harm.subs.files.derwent <- c("nber-pdp-harmonization-(derwent_standardisation_BHH-9).csv")
 
 harm.subs.files.sansderwent <- c("nber-pdp-harmonization-(punctuation2_not_all-2,5,6,10).csv"
                    , "nber-pdp-harmonization-(standard_name-8).csv"
@@ -147,12 +197,27 @@ harm.subs.files.punctuation <- "nber-pdp-harmonization-(punctuation2_not_all-2,5
 
 ## Removals
 harm.rem.files <- c("nber-pdp-harmonization-(stem_name-8).csv"
-                    ## , "nber-pdp-harmonization-(non_corporates_univ-8).csv"
+                  ## , "nber-pdp-harmonization-(non_corporates_univ-8).csv"
+                  , "additional.removals.2018-01-01.csv"
                     )
 
 ## Codes and clues for organization type
 harm.codes.files <- c("nber-pdp-harmonization-(codes_org_type-9).csv")
 
+
+## Magerman et al. (2006) harmonization
+harm.magerman <- system.file(package = "harmonizer") %>%
+    file.path("magerman-harmonization", "magerman-harmonization.csv") %>%
+    read.csv(header = FALSE, stringsAsFactors = FALSE)
+
+
+## for testing
+## harm.magerman <- file.path("..", "inst", "magerman-harmonization"
+##                          , "magerman-harmonization.csv") %>%
+##     read.csv(header = FALSE
+##            , stringsAsFactors = FALSE
+##              , strip.white = TRUE
+##            , quote = "\"")
 
 
 ## Wrapers for standardizes
@@ -161,21 +226,34 @@ harm.codes.files <- c("nber-pdp-harmonization-(codes_org_type-9).csv")
 standardize.nber <- function(org.names) {
     file.path(harm.dir, c(harm.subs.files
                         , harm.rem.files)) %>% 
-        make.subs.table %>%
         {standardize(org.names, .)}
 }
+
+
+standardize.derwent <- function(org.names) {
+    file.path(harm.dir, c(harm.subs.files.derwent)) %>% 
+        {standardize(org.names, .)}
+}
+
 
 #' @import magrittr
 standardize.nber.sansremovals <- function(org.names) {
     file.path(harm.dir, harm.subs.files) %>% 
-        make.subs.table %>%
         {standardize(org.names, .)}
 }
 
 #' @import magrittr
+#' it assumes that there is no removals for the first word
+standardize.nber.removals <- function(org.names) {
+    file.path(harm.dir, c(harm.rem.files)) %>% 
+        {standardize(org.names, .
+                   , add.spaces = "right")}
+}
+
+
+#' @import magrittr
 standardize.nber.sansderwent.sansremovals <- function(org.names) {
     file.path(harm.dir, harm.subs.files.sansderwent) %>% 
-        make.subs.table %>%
         {standardize(org.names, .)}
 }
 
@@ -183,14 +261,12 @@ standardize.nber.sansderwent.sansremovals <- function(org.names) {
 standardize.nber.sansderwent <- function(org.names) {
     file.path(harm.dir, c(harm.subs.files.sansderwent
                         , harm.rem.files)) %>% 
-        make.subs.table %>%
         {standardize(org.names, .)}
 }
 
 #' @import magrittr
 standardize.punctuation <- function(org.names) {
     file.path(harm.dir, harm.subs.files.punctuation) %>% 
-        make.subs.table %>%
         {standardize(org.names, .)}
 }
 
@@ -233,30 +309,57 @@ standardize.punctuation <- function(org.names) {
 #' @md
 harmonize <- function(org.names
                     , procedures = list(
-                          "toutf"
+                          "enc2utf8"
                         , "tolower"
-                        , "html2txt"
+                        ## , "html2txt"
                         , "clear.in.brackets"
-                        , "toascii"
-                        , "toupper"
-                        , "standardize.nber"
-                        , "trims")
-                      , quite = FALSE
+                        , list("toascii", "ind")
+                        , "toupper" 
+                        , "standardize.nber.sansremovals"
+                        , "standardize.nber.removals"
+                        , "trims"
+                      )
+                    , quite = FALSE
+                    , progress.by = 0
                       ) {
+    ## check if procedures are specified
+    if(length(procedures) == 0) {message("No harmonizing procedures are specified."); return()}
     ## Apply harmonization
     if(is.character(org.names)) {
         if(!quite) message("Running harmonizer procedures:")
         for(procedure in procedures) {
-            if(!quite) {
-                packageStartupMessage("* ", procedure[[1]], " ..."
-                                , paste0(rep("\t", 3 - (nchar(procedure[[1]]) %/% 10)))
-                                , appendLF = FALSE)
-                packageStartupMessage("DONE")
+            if(!quite) packageStartupMessage("* ", procedure[[1]], " ..."
+                                           , paste0(rep("\t"
+                                                      , 5 - ((nchar(procedure[[1]]) + 6) %/% 8)))
+                                           , appendLF = FALSE)
+            if(progress.by & !quite) {
+                i <- 1
+                l <- length(org.names)
+                n <- l %/% progress.by + 1
+                packageStartupMessage("  0%"
+                                           , appendLF = FALSE)
+                org.names %<>%
+                    split(rep(1:n
+                            , each = progress.by
+                            , length.out = l)) %>%
+                    lapply(function(org.names.by) {
+                        packageStartupMessage("\b\b\b\b", " "
+                                            , ifelse(round(100/n * i) < 10 , " ", "")
+                                            , round(100/n * i), "%"
+                                            , appendLF = FALSE)
+                        i <<- i + 1
+                        org.names.by %>% 
+                            list %>%
+                            c(procedure[-1])  %>%        # add arguents at the end
+                            {do.call(procedure[[1]], .)}
+                    }) %>% unlist
             }
-            org.names %<>% 
-                list %>%
-                c(procedure[-1]) %>%        # add arguents
-                {do.call(procedure[[1]], .)}
+            else org.names %<>% 
+                     list %>%
+                     c(procedure[-1])  %>%        # add arguents at the end
+                     {do.call(procedure[[1]], .)}
+            if(progress.by & !quite) packageStartupMessage("\b\b\b\b", "DONE")
+            if(!progress.by & !quite) packageStartupMessage("DONE")
         }
         return(org.names)
     } else {
@@ -266,10 +369,86 @@ harmonize <- function(org.names
 
 
 
-## Test
+
+
+## Testing progress bar...
+## s <- 1:7003
+## N <- 100
+## s %>% split(rep(1:(length(.) %/% N + 1)
+##             , each = N , length.out = length(.))) %>% class
+
+
+## harmonize(as.character(1:100000), progress.by = 1000) %>% class
+
+
+## Examples
+## --------------------------------------------------------------------------------
+
 ## org.names.test <- c("žŸong-ÂÃÇÈÏa\n\u00b5 &oacute;\u00b5<p>,  INt LTD &AMP; Co;  "
 ##                   , "<br> the $ (&AMP; C&oacute;MP comPANY) Ïotta")
 ## data.frame(original = org.names.test
 ##          , harmonized = harmonize(org.names.test))
 
 
+## comp.example <- c("žŸong-ÂÃÇÈÏa\n\u00b5 &oacute;\u00b5<p>,  INt LTD &AMP; Co;  "
+##                 , "<br> the $ (&AMP; C&oacute;MP comPANY) Ïotta"
+##                 , "Tempshield Cryo-Protection™"
+##                 , "Ábcdêãçoàúü"
+##                 , "Polgen Sp. z o.o. <U+0096> Sp. K."
+##                 , "Polgen Sp. z o.o. – Sp. K."
+##                 , "Jerome® <br>"
+##                 , "Controlled Environments®  Magazine"
+##                 , "a\n\u00b5\u00b5"
+##                 , "fa\xE7ile"
+##                 , "fa\xc3\xa7ile"
+##                 , "MSlab CO. CO., LTD."
+##                 , "MSlab, A \\SOCIETE ANONYME\\"
+##                 , "S.A.S. University Co., {PE}, Ltd. (Europe)"
+##                 , "Analytical Technologies Limited"
+##                 , "Anasys Instruments Corporation"
+##                 , "C4 Control de Contaminacion Ltda"
+##                 , "Crescent Scientific Pvt Ltd."
+##                 , "Daigger & Co., Inc."
+##                 , "Dell Inc."
+##                 , "Deltalab. S.L.U."
+##                 , "DLAB Scientific Co.,Ltd."
+##                 , "ebro Electronic GmbH und Co. KG"
+##                 , "Ecom spol. s r.o., s.r.o., akc. spol."
+##                 , "G.A.S. mbH"
+##                 , "Glassco Laboratory Equipments PVT LTD"
+##                 , "Lhasa Limited"
+##                 , "rose plastic USA, LLLP")
+
+
+## comp.example %>% harmonize(procedures = list(
+##                                "enc2utf8"
+##                                 , "tolower"
+##                                  ## , "html2txt"
+##                                , "clear.in.brackets"
+##                                 , list("toascii", FALSE)
+##                                , "toupper" 
+##                                , "standardize.nber.sansremovals"
+##                                , "standardize.nber.removals"
+##                                , "trims"
+##                           ))
+
+
+## data.frame(origin = (harm.magerman[[1]] %>%
+##                      {paste0("MSlab", .)})
+##          , harm = (harm.magerman[[1]] %>%
+##                    {paste0("MSlab", .)} %>%
+##                    harmonize(procedures = list(
+##                                  "toutf"
+##                                , "tolower"
+##                                  ## , "html2txt"
+##                                , "clear.in.brackets"
+##                                , "toascii"
+##                                , "toupper"
+##                                , "standardize.nber"
+##                                , "standardize.nber.sansremovals"
+##                                , "standardize.nber.removals"
+##                                , "trims"
+##                              )))
+##          , magerman = (harm.magerman[[3]] %>% 
+##                        {paste0("MSlab", .)})) %>%
+##     write.csv("test.csv")
